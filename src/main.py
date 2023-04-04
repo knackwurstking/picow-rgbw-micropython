652f9ae3173e0ec5309628238a2114723cafdf50
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-04 11:26:07 +0200

    clean up and remove print statements

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import contextlib
 import socket
 from time import sleep
 
+import gc
+import micropython
 import machine
 import network
 from picozero import pico_led
@@ -15,11 +17,11 @@ def connect():
     """Connect to WLAN (ssid, password)"""
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
+    wlan.config(pm = 0xa11140)  # disable power-save mode
     wlan.connect(info.SSID, info.PASSWORD)
 
     # Wait for connection
     while wlan.isconnected() is False:
-        print("Waiting for wlan connection...")
         sleep(1)
 
     return wlan.ifconfig()[0]
@@ -37,7 +39,10 @@ def open_socket(ip):
 def serve(c):
     """Start the web server"""
     pico_led.on()
+    gc.collect()
+    gc.disable()
     while True:
+        micropython.mem_info()
         client = c.accept()[0]
 
         try:
@@ -48,12 +53,10 @@ def serve(c):
                 client.send(body)
         finally:
             client.close()
+            gc.collect()
 
 
 def handle_request(req: str):
-    header: str = "HTTP/1.0 404 NOT FOUND\r\nContent-Type: text/text\r\n\r\n"
-    body: str = ""
-
     method: str = ""
     pathname: str = ""
     query: list[str] = []
@@ -66,32 +69,25 @@ def handle_request(req: str):
             query = query_split[1].split("&")
         pathname = query_split[0]
 
-    print(method, "|", pathname, "|", query)
+    if pathname[:13] == "/rgbw/set_pin" and method == "POST":
+        return rgbwhandler.set_pin(parse_query(query))
 
-    # POST: "/rgbw/set_pin" ? r=<1-28> & g=<1-28> & b=<1-28> & w=<1-28>
-    if pathname.startswith("/rgbw/set_pin") and method.upper() == "POST":
-        header, body = rgbwhandler.set_pin(parse_query(query))
+    if pathname[:13] == "/rgbw/set_pwm" and method == "POST":
+        return rgbwhandler.set_pwm(parse_query(query))
 
-    # POST: "/rgbw/set_pwm" ? r=<0-100%> & g=<0-100%> & b=<0-100%> & w=<0-100%>
-    elif pathname.startswith("/rgbw/set_pwm") and method.upper() == "POST":
-        header, body = rgbwhandler.set_pwm(parse_query(query))
+    if pathname[:14] == "/rgbw/get_pins" and method == "GET":
+        return rgbwhandler.get_pins()
 
-    # GET: "/rgbw/get_pins"
-    elif pathname.startswith("/rgbw/get_pins") and method.upper() == "GET":
-        header, body = rgbwhandler.get_pins()
+    if pathname[:14] == "/rgbw/get_duty" and method == "GET":
+        return rgbwhandler.get_duty()
 
-    # GET: "/rgbw/get_duty"
-    elif pathname.startswith("/rgbw/get_duty") and method.upper() == "GET":
-        header, body = rgbwhandler.get_duty()
+    if pathname[:7] == "/device" and method == "GET":
+        return handler.device()
 
-    # GET: "/device" - device name
-    elif pathname.startswith("/device") and method.upper() == "GET":
-        header, body = handler.device()
+    if pathname[:1] == "/" and method == "GET":
+        return handler.info_page()
 
-    elif pathname == "/" and method.upper() == "GET":
-        header, body = handler.info_page()
-
-    return header, body
+    return "HTTP/1.0 404 NOT FOUND\r\nContent-Type: text/text\r\n\r\n", ""
 
 
 def parse_query(queries: list[str]):
@@ -106,7 +102,6 @@ def parse_query(queries: list[str]):
 
 try:
     info.IP = connect()
-    print(f"WIFI connected on {info.IP}")
 
     c = open_socket(info.IP)
     try:
