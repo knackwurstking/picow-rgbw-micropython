c119bcb08d48240b436f8894e61190e0b69260b8
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-08 01:30:47 +0200

    Simplify post server func

--- src/handler/server.py
+++ src/handler/server.py
@@ -13,19 +13,13 @@ def get():
 def post(query: dict[str, str]):
     status = "200 OK"
 
-    if query.get("protocol") not in ["http:", "https:"]:
-        status = "400 BAD REQUEST"
-    else:
+    if query.get("protocol") in ["http:", "https:"]:
         config.SERVER["protocol"] = query.get("protocol")
 
-    if not query.get("host"):
-        status = "400 BAD REQUEST"
-    else:
+    if query.get("host"):
         config.SERVER["host"] = query.get("host")
 
-    if not query.get("port"):
-        status = "400 BAD REQUEST"
-    else:
+    if query.get("port"):
         try:
             config.SERVER["port"] = int(query["port"])
         except Exception:
