d969519ab8d0eeaee86378023b16ef6a1d970db5
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-07 23:34:25 +0200

    Add post_server function

--- src/handler/config.py
+++ src/handler/config.py
@@ -9,12 +9,27 @@ def get_server():
     )
 
 
-def post_server():
+def post_server(query: dict[str, str]):
     status = "200 OK"
     body = ""
 
-    # TODO: read body data (content-type: "text/text")
-    # TODO: validate data (protocol, host and port)
-    # TODO: store and register on server `config.register_to_server()`
+    if query["protocol"] not in ["http:", "https:"]:
+        status = "400 BAD REQUEST"
+
+    if not query["host"]:
+        status = "400 BAD REQUEST"
+
+    if not query["port"]:
+        status = "400 BAD REQUEST"
+    else:
+        try:
+            int(query["port"])
+        except Exception:
+            status = "400 BAD REQUEST"
+
+    if status != "200 OK":
+        config.SERVER["protocol"] = query["protocol"]
+        config.SERVER["host"] = query["host"]
+        config.SERVER["port"] = int(query["port"])
 
     return utils.response(status, body)
