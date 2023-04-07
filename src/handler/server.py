ec3cea019e47c40c25135c4a8ad41028baf38e9c
Udo Bauer <knackwurstking.tux@gmail.com>
2023-04-08 01:28:18 +0200

    Update post server func

--- src/handler/server.py
+++ src/handler/server.py
@@ -11,33 +11,27 @@ def get():
 
 
 def post(query: dict[str, str]):
-    print(query, config.SERVER)
     status = "200 OK"
 
-    if query["protocol"] not in ["http:", "https:"]:
+    if query.get("protocol") not in ["http:", "https:"]:
         status = "400 BAD REQUEST"
+    else:
+        config.SERVER["protocol"] = query.get("protocol")
 
-    if not query["host"]:
+    if not query.get("host"):
         status = "400 BAD REQUEST"
+    else:
+        config.SERVER["host"] = query.get("host")
 
-    if not query["port"]:
+    if not query.get("port"):
         status = "400 BAD REQUEST"
     else:
         try:
-            int(query["port"])
+            config.SERVER["port"] = int(query["port"])
         except Exception:
             status = "400 BAD REQUEST"
 
     if status == "200 OK":
-        if query["protocol"]:
-            config.SERVER["protocol"] = query["protocol"]
-
-        if query["host"]:
-            config.SERVER["host"] = query["host"]
-
-        if query["port"]:
-            config.SERVER["port"] = int(query["port"])
-
         config.save()
 
     return utils.response(status)
