b6da4ea22d49adf489a77ccbc781ca0b97980b0e
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-10 16:41:56 +0200

    Adding device request handlers (pwm range and freq, server get/set)

--- src/handler/device.py
+++ src/handler/device.py
@@ -1,14 +1,22 @@
-def device_pwm_range(args: list[str]) -> None:
+import config
+
+
+def device_pwm_range(_args: list[str]) -> str:
     """..."""
+    return "0 100"
 
 
-def device_pwm_freq(args: list[str]) -> None:
+def device_pwm_freq(_args: list[str]) -> str:
     """..."""
+    return f"{config.PWM_FREQ}"
 
 
-def device_server_get(args: list[str]) -> None:
+def device_server_get(_args: list[str]) -> str:
     """..."""
+    return config.SERVER
 
 
 def device_server_set(args: list[str]) -> None:
     """..."""
+    if len(args) == 1:
+        config.SERVER = args[0]
