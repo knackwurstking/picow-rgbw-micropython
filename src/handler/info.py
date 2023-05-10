c45944c97f535cf3d9bc9e1173756f5b65b734dd
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-10 16:23:30 +0200

    adding info_disk_usage and info_temp handler functions

--- src/handler/info.py
+++ src/handler/info.py
@@ -1,6 +1,22 @@
-def info_temp(args: list[str]) -> None:
-    """..."""
+import os
+
+from picozero import pico_temp_sensor
 
 
-def info_disk_usage(args: list[str]) -> None:
+def info_temp(_args: list[str]) -> str:
     """..."""
+    return f"{pico_temp_sensor.temp}"
+
+
+def info_disk_usage(_args: list[str]) -> str:
+    """..."""
+    disk = os.statvfs("/")
+
+    block_size = disk[0]
+    total_blocks = disk[2]
+    free_blocks = disk[3]
+
+    used = (block_size * total_blocks) - (block_size * free_blocks)
+    free = block_size * free_blocks
+
+    return f"{used} {free}"
