218cbc2ab5892d2733396944d585b7837f52beb3
Udo Bauer <knackwurstking.tux@gmail.com>
2023-05-12 01:12:53 +0200

    remove os module

--- src/log.py
+++ src/log.py
@@ -1,26 +1,16 @@
-import os.path
-
 import config
 
 
 def get() -> str:
     """Get pico log"""
-    if not os.path.exists("pico.log"):
-        return ""
-
     with open("pico.log", "r", encoding="utf-8") as file:
         return file.read()
 
 
 def clear():
     """Remove all stuff from the pico.log file"""
-    if not os.path.exists("pico.log"):
-        return None
-
     with open("pico.log", "w", encoding="utf-8") as file:
         file.write("")
-
-    return None
 
 
 def log(message):
